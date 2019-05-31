b46d5a19a9a1cd50ec9e654f3520ee6963a922d2
Asha <ashalbenjamin@gmail.com>
2019-05-30 22:11:30 -0500

    changed-super-to-attr-added-project-manager

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,10 +1,10 @@
 // CODE here for your Lambda Classes
 //Class Person
 class Person {
-	constructor(name, age, location){
-		this.name = name;
-		this.age = age;
-		this.location = location;
+	constructor(attr){
+		this.name = attr.name;
+		this.age = attr.age;
+		this.location = attr.location;
 	}
 	speak() {
 		console.log(`Hello my name is ${this.name}, I am from ${this.location}.`)
@@ -12,7 +12,47 @@ class Person {
 	}
 }
 
-const JellyJack = new Person('Jack', 35, 'Austin');
+//Class Student
+	class Student extends Person {
+		constructor(previousBackground, className, favSubjects) {
+			super(name, age, location);
+			this.previousBackground = previousBackground;
+			this.className = className;
+			this.favSubjects = favSubjects;
+		}
+
+		listsSubjects() {
+
+			console.log(`This is a list of ${this.name}'s favorite subjects.`);
+			let subArray = [];
+			let item = 0;
+			subArray = this.favSubjects;
+			for(item in subArray) {
+				console.log(`subject: ${subArray[item]}`);
+			}
+
+		}
+
+		PRAssignment(student, subject) {
+			console.log(`${student} has submitted a PR for ${subject}.`);
+		}
+
+		sprintChallenge(student, subject) {
+			console.log(`${student} has begun sprint challenge on ${subject}.`);
+		}
+	}
+
+	const fred = new Student({
+  name: 'Fred',
+  location: 'Bedrock',
+  age: 37,
+  previousBackground: 'Science Teacher',
+  className: 'WebPT07',
+  favSubjects: ['Illustration','Biology','C#']
+});
+
+
+//Class Instructor
 	class Instructor extends Person {
 		constructor(specialty, favLanguage, catchPhrase){
 			super(name,age,location);
@@ -22,34 +62,50 @@ const JellyJack = new Person('Jack', 35, 'Austin');
 		}
 
 		demo(subject) {
-			console.log(`Today we are learning about ${this.subject}`);
+			console.log(`Today we are learning about ${subject}.`);
 		}
-		grade(student, subject){
-			console.log(`${student.name} receives a perfect score ${subject} `);
-		}
-	}
-	//Class Student
-	class Student extends Person {
-		constructor(previousBackground, className, favSubjects) {
-			super(name, age, location);
-			this.previousBackground = previousBackground;
-			this.className = className;
-			this.favSubjects = favSubjects;
-		}
-
-		listsSubjects(favSubjects) {
-
-			console.log(this.favSubjects);
-		}
-
-		PRAssignment(subject) {
-			console.log(`${student.name} has submitted a PR for ${subject}`);
-		}
-
-		springChallenge(subject) {
-			console.log(`${student.name} has begun sprint challenge on ${subject}`);
+		grade(student, subject) {
+			console.log(`${student} receives a perfect score ${subject} `);
 		}
 	}
-	// //Test
-	// const JellyJack = new Person('Jack', 35, 'Austin');
-	JellyJack.speak();
+	
+	const Jack = new Instructor({
+		name: 'Jack',
+		location: 'Houston',
+		age: 26,
+		specialty: 'Singing',
+		favLanguage: 'Python',
+		catchPhrase: 'whasssuuupppp!?'
+	});
+
+	// Project Manager
+	class ProjectManager extends Person {
+		constructor(personAttributes) {
+			super(personAttributes);
+			this.gradClassName = personAttributes.gradClassName;
+			this.favInstructor = personAttributes.favInstructor;
+		}
+		standUp(channel){
+		console.log(`${this.name} announces to ${channel}, @channel standy times!`);
+		}
+
+		debugsCode(student, subject) {
+			console.log(`${name} debugs ${student.name}'s code on ${subject}.'`)
+		}
+	}
+	const Jill = new ProjectManager({
+		name: 'Jill',
+		location: 'Arlington',
+		age: 32,
+		gradClassName: 'WebPT07',
+		favInstructor: 'Josh'
+	});
+
+	fred.speak();
+	fred.listsSubjects();
+	fred.sprintChallenge(fred.name, fred.favSubjects[0]);
+	Jack.demo(fred.favSubjects[1]);
+	Jack.grade(fred.name,fred.favSubjects[2]);
+	console.log(Jack.catchPhrase);
+	Jill.standUp(fred.className);
+	Jill.debugsCode(fred.name,fred.favSubjects[2]);
